bd4b0827591212d4d0200f51ce8ae5175c859605
Paul Martinetti <paul@digscitech.com>
2019-08-18 14:36:22 +0200

    animates and counts 0 to 9

--- js/scenes/gameScene.js
+++ js/scenes/gameScene.js
@@ -7,6 +7,26 @@ gameScene.init = function () {
     this.gameW = this.sys.game.config.width;
     this.gameH = this.sys.game.config.height;
 
+    this.moving = true;
+
+    this.movesA = [
+        {
+            sx: 100,
+            sy: 100,
+            fx: 350,
+            fy: 100
+        },
+        {
+            sx: 100,
+            sy: 350,
+            fx: 100,
+            fy: 100
+        }
+    ];
+
+    this.ind = 0;
+    this.min = 0;
+
 };
 
 // executed once, after assets were loaded
@@ -15,8 +35,8 @@ gameScene.create = function () {
     //console.log('here');
 
     // ajoute droite
-    let droite = this.add.sprite(100, 100, 'droite').setOrigin(0,0);
-    droite.setFrame(4);
+    this.droite = this.add.sprite(100, 100, 'droite').setOrigin(0, 0);
+    this.droite.setFrame(0);
 
     // mask to animate
     let dMask = this.add.graphics().setVisible(false);
@@ -25,81 +45,57 @@ gameScene.create = function () {
     // params - x, y, h, w
     dMask.fillRect(100, 100, 250, 250);
     // add mask to droit
-    droite.mask = new Phaser.Display.Masks.BitmapMask(this, dMask);
+    this.droite.mask = new Phaser.Display.Masks.BitmapMask(this, dMask);
 
-    // right exit animation
-    let sortDroite = this.tweens.add({
-        targets: droite,
-        duration: 1500,
-        x: 350,
-        y: 100,
-        paused: false,
-        callbackScopte: this,
-        onComplete: function (tween, sprites) {
-            console.log('done');
-        }
-
-    });
+    this.moving = false;
 
 
     // to start in the middle of the grid, have to use update() and limit
 };
-// fn context = Scene not Sprite (bg) passed 'this' in on();
-gameScene.placeItem = function (pointer, localX, localY) {
-    // var 'pointer' shows game scene coordinates
-    // vars localX, localY show coordinates of object selected
-    // with our bg, it's the same in this case
-    //console.log(localX, localY);
+gameScene.update = function () {
 
-    // check for selected item, otherwise it's just a bg click
-    if (!this.selectedItem) return;
+    console.log(this.ind);
+    
+ // move
+    if (!this.moving && this.min<10) {
+        console.log('here');
+        this.move(this.droite, this.movesA[this.ind]);
+    }
+    
+}
 
-    // ui must be unblocked
-    //if(if.uiBlocked) return;
 
-    // create a new item in the position where user clicked
-    let newItem = this.add.sprite(localX, localY, this.selectedItem.texture.key)
+gameScene.move = function (spr, obj) {
 
-    // block UI while pet goes to eat selectedItem
-    this.uiBlocked = true;
+    // set x, y
+    spr.x = obj.sx;
+    spr.y = obj.sy;
 
-    // move this.pet to newItem (set in create fn)
-    // onComplete is the callback fn
-    // tween is inside scene context (passed 'this' from btn)
-    let petTween = this.tweens.add({
-        targets: this.pet,
-        duration: 500,
-        x: newItem.x,
-        y: newItem.y,
+    // start
+    this.moving = true;
+
+    // right exit animation
+    let sortDroite = this.tweens.add({
+        targets: spr,
+        duration: 1500,
+        x: obj.fx,
+        y: obj.fy,
         paused: false,
         callbackScope: this,
         onComplete: function (tween, sprites) {
-
-            // make newItem disappear
-            newItem.destroy();
-
-            // listen for chewing to finish before unlocking ui
-            this.pet.on('animationcomplete', function () {
-
-                // put pet face back to frame 0
-                // use setFrame after obj is created (instead of .frame())
-                this.pet.setFrame(0);
-
-                // to limit placing one item, null selectedItem, reset ui
-                // this must follow stats update that needs selectedItem
-                this.uiReady();
-                // pass scene context (this)
-            }, this);
-
-            // newItem is reached, so play chewing animation
-            this.pet.play('funnyfaces');
-
-            // update stats (final implementation)
-            this.updateStats(this.selectedItem.customStats);
-
+            this.moving = false;
+            
+            this.ind++;
+            if (this.ind > 1) {
+                this.ind = 0;
+            } else {
+                this.min++;
+                this.droite.setFrame(this.min);
+            }
+            
         }
-    });
-};
 
+    }, this);
+}
 
 // no code here, doesn't run
