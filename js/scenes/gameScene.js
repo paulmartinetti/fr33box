4309796112f18efe70c9fb29b6b4ff488b2f67ea
Paul Martinetti <paul@digscitech.com>
2019-08-19 11:28:30 +0200

    mask function, two tweens set

--- js/scenes/gameScene.js
+++ js/scenes/gameScene.js
@@ -7,68 +7,26 @@ gameScene.init = function () {
     this.gameW = this.sys.game.config.width;
     this.gameH = this.sys.game.config.height;
 
-    // globals
-    let side = 250;
-    let space = 25;
-    this.multi = 1;
-
-    // gameH = 1000, each number is 250, so we start in the middle
-    this.format = [
-        {
-            sx: space,
-            sy: space,
-            fx: -1 * side,
-            fy: space,
-            rx: space,
-            ry: -1 * side
-        },
-        {
-            sx: (2 * space) + side,
-            sy: space,
-            fx: (2 * space) + (2 * side),
-            fy: space,
-            rx: (2 * space) + side,
-            ry: -1 * side
-        },
-        {
-            sx: space,
-            sy: (2 * space) + side,
-            fx: -1 * side,
-            fy: (2 * space) + side,
-            rx: space,
-            ry: (2 * space) + (2 * side)
-        },
-        {
-            sx: (2 * space) + side,
-            sy: (2 * space) + side,
-            fx: (2 * space) + (2 * side),
-            fy: (2 * space) + side,
-            rx: (2 * space) + side,
-            ry: (2 * space) + (2 * side)
-        }
-    ];
+    this.margin = 25;
+    this.spacer = 25;
+    this.side = 250;
+    this.fois = 1;
 
 };
 
 // executed once, after assets were loaded
 gameScene.create = function () {
 
-    // add hour left (0, 1, 2)
-
-
     // ajoute droite
-    this.htens = this.add.sprite(100, 100, 'droite').setOrigin(0, 0);
-    this.htens.setFrame(0);
-    // mask to animate
-    let dMask = this.add.graphics().setVisible(false);
-    // enter color first, then alpha value
-    dMask.fillStyle(0x000000, 1);
-    // params - x, y, h, w
-    dMask.fillRect(100, 100, 250, 250);
-    // add mask to droit
-    this.htens.mask = new Phaser.Display.Masks.BitmapMask(this, dMask);
+    this.htens = this.add.sprite(25, 25, 'droite').setOrigin(0, 0);
+    //this.htens.setFrame(0);
+    // params - x and y of mask
+    this.htens.mask = this.maskMe(this.htens.x,this.htens.y);
 
-    this.timedEventStats = this.time.addEvent({
+    // test move
+    this.depart(this.htens);
+
+    /* this.timedEventStats = this.time.addEvent({
         delay: 1000,
         repeat: -1,
         callback: function () {
@@ -76,13 +34,10 @@ gameScene.create = function () {
             this.updateTime();
         },
         callbackScope: this
-    });
-
-    //
-    this.move(this.htens, this.format[0]);
-
+    }); */
     // to start in the middle of the grid, have to use update() and limit
 };
+
 gameScene.updateTime = function () {
     var time = new Date();
 
@@ -104,44 +59,85 @@ gameScene.updateTime = function () {
 
 }
 
-gameScene.move = function (spr, obj) {
-
-    // set x, y
-    spr.x = obj.sx;
-    spr.y = obj.sy;
-
-    // right exit animation--- REPLACE W TIMELINE
-    let sortDroite = this.tweens.add({
+gameScene.depart = function (spr) {
+    let depart = this.tweens.add({
         targets: spr,
         duration: 1500,
-        x: obj.fx,
-        y: obj.fy,
+        x: -225,
+        y: 25,
         paused: false,
         callbackScope: this,
         onComplete: function (tween, sprites) {
-
-            this.droite.setFrame(1);
-
+            spr.setFrame(1);
             // set x, y
-            spr.x = obj.rx;
-            spr.y = obj.ry;
-            
-            let rentre = this.tweens.add({
-                targets: spr,
-                duration: 1500,
-                x: obj.sx,
-                y: obj.sy,
-                paused: false,
-                callbackScope: this,
-                onComplete: function (tween, sprites) {
-                    console.log('done');
-                }
-
-            })
-
+            spr.x = 25;
+            spr.y = -225;
+            this.rentre(spr);
         }
-
     }, this);
 }
+gameScene.rentre = function (spr) {
+    let rentre = this.tweens.add({
+        targets: spr,
+        duration: 1500,
+        x: 25,
+        y: 25,
+        paused: false,
+        callbackScope: this,
+        onComplete: function (tween, sprites) {
+            console.log('here')
+        }
+    }, this);
+}
+gameScene.maskMe = function (ix, iy) {
+    // mask to animate
+    let dMask = this.add.graphics().setVisible(false);
+    // enter color first, then alpha value
+    dMask.fillStyle(0x000000, 1);
+    // params - x, y, h, w
+    dMask.fillRect(ix, iy, this.side, this.side);
+    // add mask to droit
+    return new Phaser.Display.Masks.BitmapMask(this, dMask);
+}
 
-// no code here, doesn't run
+/******************* no code below here, doesn't run */
+// globals
+let side = 250;
+let space = 25;
+this.multi = 1;
+
+// gameH = 1000, each number is 250, so we start in the middle
+this.format = [
+    {
+        sx: space,
+        sy: space,
+        fx: -1 * side,
+        fy: space,
+        rx: space,
+        ry: -1 * side
+    },
+    {
+        sx: (2 * space) + side,
+        sy: space,
+        fx: (2 * space) + (2 * side),
+        fy: space,
+        rx: (2 * space) + side,
+        ry: -1 * side
+    },
+    {
+        sx: space,
+        sy: (2 * space) + side,
+        fx: -1 * side,
+        fy: (2 * space) + side,
+        rx: space,
+        ry: (2 * space) + (2 * side)
+    },
+    {
+        sx: (2 * space) + side,
+        sy: (2 * space) + side,
+        fx: (2 * space) + (2 * side),
+        fy: (2 * space) + side,
+        rx: (2 * space) + side,
+        ry: (2 * space) + (2 * side)
+    }
+];
